9745666362b12faa5b36558772a1e028a0d46668
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2023-10-24 14:45:20 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [joke, setJoke] = useState('');
+  const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+  let getJoke = () => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setJoke(data.joke);
+    });
+  }
+
+  useEffect(() => {
+    getJoke();
+  }, [])
+  
+  const handleClick = () => {
+    getJoke();
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="wrapper">
+        <h1>AWSM JOKES</h1>
+        <span>&#128514;</span>
+        <p id=" ">{joke}</p>
+        <button id="btn" onClick={handleClick}>
+          Get Randome Joke
+        </button>
+      </div>
     </div>
   );
 }
